1770d490526ccfbc20b9d8b5df13c16d49049621
b3ck3rz <b3ck3rzzz@gmail.com>
2022-01-26 18:56:01 +0300

    adding grayscale

--- main.py
+++ main.py
@@ -3,20 +3,36 @@ from PIL import Image
 
 filename = sys.argv[1]
 
-img = Image.open(filename).convert('1')
+img = Image.open(filename).convert('L')
 img.load()
 width, height = img.size
 matrix = [[0 for _ in range(width)] for _ in range(height)]
 
 for x in range(width):
     for y in range(height):
-        rgb = img.getpixel((x, y))
-        if rgb == 0:
-            matrix[y][x] = 0
+        color = img.getpixel((x, y))
+        if color < 25:
+            matrix[y][x] = '@'
+        elif color < 50:
+            matrix[y][x] = '%'
+        elif color < 75:
+            matrix[y][x] = '#'
+        elif color < 100:
+            matrix[y][x] = '*'
+        elif color < 125:
+            matrix[y][x] = '+'
+        elif color < 150:
+            matrix[y][x] = '='
+        elif color < 175:
+            matrix[y][x] = '-'
+        elif color < 200:
+            matrix[y][x] = ':'
+        elif color < 255:
+            matrix[y][x] = '.'    
         else:
-            matrix[y][x] = '.'
+            matrix[y][x] = ' '
 
-[print(*row) for row in matrix]
+[print(*row, sep='') for row in matrix]
 
 
 
